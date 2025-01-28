e528a8292aa13222bd71cd96b9a2a3b38e7700d6
Ozias Costa <oziasmcn@gmail.com>
2025-01-27 21:07:00 -0300

    Inicio das classes de dominio

---
+++ src/GestaoContas.Business/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace GestaoContas.Business.Models
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
