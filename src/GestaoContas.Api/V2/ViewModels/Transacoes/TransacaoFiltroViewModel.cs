7eee759ff13489afcee586b0f224888e251ebd29
Ozias Costa <oziasmcn@gmail.com>
2025-02-13 22:13:16 -0300

    Busca por transações

---
+++ src/GestaoContas.Api/V2/ViewModels/Transacoes/TransacaoFiltroViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GestaoContas.Business.Models;
+
+namespace GestaoContas.Api.V2.ViewModels.Transacoes
+{
+    public class TransacaoFiltroViewModel
+    {
+        public DateTime? DataInicial { get; set; }
+        public DateTime? DataFinal { get; set; }
+        public decimal? ValorInicial { get; set; }
+        public decimal? ValorFinal { get; set; }
+        public Guid? CategoriaId { get; set; }
+        public TipoTransacao? Tipo { get; set; }
+    }
+}
