44607fa8a49f3e7d894cbfb8797f02554dde07ec
Victor Lino <victor.lino@treeinova.com.br>
2025-03-10 19:13:30 -0300

    dash

--- src/front/apps/gestao-compra/src/app/authenticated/inicio/inicio.module.ts
+++ src/front/apps/gestao-compra/src/app/authenticated/inicio/inicio.module.ts
@@ -4,20 +4,27 @@ import { MatInputModule } from "@angular/material/input";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { DashboardComponent } from "./dashboard/dashboard.component";
 import { InicioComponent } from "./inicio.component";
 import { NgxEchartsModule } from "ngx-echarts";
+import { MAT_DATE_LOCALE, MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { CommonModule } from "@angular/common";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { DashboardService } from "@front/services";
 
 @NgModule({
   imports: [InicioRouting,
+    CommonModule,
     MatInputModule,
     ReactiveFormsModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
     NgxEchartsModule.forRoot({echarts: () => import('echarts')})
   ],
   exports: [],
   declarations: [InicioComponent, DashboardComponent],
-  providers: [],
+  providers: [DashboardService, {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}],
 })
 export class InicioModule { }
